f331a2c0dd190b10de94b1b24236cb0a1a929847
unknown <solyabay@gmail.com>
2023-06-20 16:23:21 +0600

    Created text

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
         mainMethodShowInfo();
     }
     private static void mainMethodShowMessageAble() {
-        System.out.println("Hello able QA course people\n");
+        System.out.println("Hello able QA course\n");
     }
     private static void mainMethodShowInfo() {
         System.out.println("new changes");
